948df45928574bb3a7ef5f874dd8e1f6ea7dbeb6
tmalik1 <39743728+tmalik1@users.noreply.github.com>
2019-11-07 16:55:44 -0500

    lab8

--- lab_8/server.js
+++ lab_8/server.js
@@ -46,22 +46,22 @@ app.get('/api', (req, res) => {
   fetch(baseURL)
     .then((r) => r.json())
     .then((data) => {
-     let coursearr = [];
-     let namearr = [];
-     let newdata = [];
-     data = data.filter((courses) => courses.dept_id === 'INST');
-     namearr = data.map((courses) => courses.name);
-     coursearr = data.map((courses) => courses.course_id);
-     for(let i=0; i<namearr.length; i++) {
-       newdata[i] = coursearr[i] + ": " + namearr[i];
-     }
-     data = newdata;
-     res.send({ data: data });
-   })
-   .catch((err) => {
-     console.log(err);
-     res.redirect('/error');
-   });
+      let coursearr = [];
+      let namearr = [];
+      let newdata = [];
+      data = data.filter((courses) => courses.dept_id === 'INST');
+      namearr = data.map((courses) => courses.name);
+      coursearr = data.map((courses) => courses.course_id);
+      for(let i=0; i<namearr.length; i++) {
+        newdata[i] = coursearr[i] + ": " + namearr[i];
+      }
+      data = newdata;
+      res.send({ data: data });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.redirect('/error');
+    });
 });
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
